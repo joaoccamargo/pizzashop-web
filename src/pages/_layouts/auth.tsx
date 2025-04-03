ca87f5590cdb9f9d152685318278a39f48964bb0
João Camargo <jclaudio00_12@hotmail.com>
2025-04-03 20:29:48 -0300

    installed ui, theme and others...

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,11 +1,20 @@
+import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Autenticação</h1>
+    <div className="grid min-h-screen grid-cols-2 antialiased">
+      <div className="border-foreground/5 bg-muted text-muted-foreground flex h-full flex-col justify-between border-r p-10">
+        <div className="text-foreground flex items-center gap-3 text-lg font-medium">
+          <Pizza className="h-8 w-8" />
+          <span className="font-semibold">pizza.shop</span>
+        </div>
+        <footer className="text-sm">
+          Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
+        </footer>
+      </div>
 
-      <div>
+      <div className="relative flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,7 +3,9 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
+import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter([
   {
@@ -14,16 +16,24 @@ export const router = createBrowserRouter([
         path: '/',
         element: <Dashboard />,
       },
+      {
+        path: '/orders',
+        element: <Orders />,
+      },
     ],
   },
 
   {
-    path: '/sign-in',
+    path: '/',
     element: <AuthLayout />,
     children: [
       {
         path: '/sign-in',
         element: <SignIn />,
+      },
+      {
+        path: '/sign-up',
+        element: <SignUp />,
       },
     ],
   },
